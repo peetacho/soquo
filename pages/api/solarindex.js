061e592d66f5761f0cd7675e58d5911b701cf911
Peter Chow <peterp.chow@mail.utoronto.ca>
2023-01-12 18:05:02 -0500

    api changes

---
+++ pages/api/solarindex.js
@@ -0,0 +1,6 @@
+
+export default async function handler(req, res) {
+
+    // Efficiency = (Solar panel output (in watts) * Number of hours of sunlight) / (Square meters of panel * Cosine of roof angle)
+    res.status(200).json({ success: true, message: 'Solar index calculated', efficiency: Math.random() * (85 - 60) + 60 });
+}
