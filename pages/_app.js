ed5a40705ffeee331ae6a1c3cbcdb3fa4b32429c
Peter Chow <peterp.chow@mail.utoronto.ca>
2023-01-13 16:47:41 -0500

    added title

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../lib/theme'
 import "@fontsource/open-sauce-one"
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <div>
+      <Head>
+        <title>SoQuo</title>
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </div>
   )
 }
 
